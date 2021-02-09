77c200527f5410694629ebb4f74439afdac443d7
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-02-09 18:57:05 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -51,7 +51,7 @@ const VisitedAgainLaunchRequestHandler = {
             dishesStatus = 'clean';
             speakOutputPlus = 'Are they dirty now?';
         }
-        const speakOutput = 'Welcome back. I remember your dishes were ' + dishesStatus;
+        speakOutput = 'Welcome back. I remember your dishes were ' + dishesStatus;
         const repromptText = 'Yes or no.' + speakOutputPlus;
 
         if (dishesStatus == '') {
